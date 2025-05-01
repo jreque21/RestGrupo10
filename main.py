8a953d14002c1843e500a0a1861c3317d3106941
jreque21 <johnnyreque@gmail.com>
2025-05-01 13:02:41 -0500

    Update main

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if not firebase_admin._apps:
 db = firestore.client()
 
 # Cargar modelo de Hugging Face
-model_name = "jreque/Llama-3.2"
+model_name = "jreque21/Llama-3.2"
 tokenizer  = AutoTokenizer.from_pretrained(model_name)
 device     = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model      = AutoModelForCausalLM.from_pretrained(
